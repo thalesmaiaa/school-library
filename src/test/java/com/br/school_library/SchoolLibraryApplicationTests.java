3f8efe204297b2d760f701739b7f035336c05786
Thales Maia <thalesmaiaalexandre@gmail.com>
2025-04-28 20:32:09 -0300

    initial commit

---
+++ src/test/java/com/br/school_library/SchoolLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.school_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
